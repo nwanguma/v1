2cd0c3f93482c5c5fa57db0b1f35e8e7e5f05e79
Tochukwu Nwanguma <Tochukwu@cico.ng>
2020-06-09 01:27:50 +0100

    Add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,17 @@ module.exports = {
         display: "swap",
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-168846842-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    }
     // {
     //   resolve: `gatsby-source-medium`,
     //   options: {
